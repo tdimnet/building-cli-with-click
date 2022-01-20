d5c771a77ead58364d5d003907fdaf04eae4c2dc
tdimnet <thomas.dimnet@datadoghq.com>
2022-01-20 17:12:40 +0100

    Update the database commands

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ python -m pytest
 
 - https://realpython.com/command-line-interfaces-python-argparse/
 
+
+## Review
+
+- Finite state machime (FSM)
+  - Do one thing
+  - Them procces it
+  - 3 states here for instance
+
+
--- store_inventory/commands/database.py
+++ store_inventory/commands/database.py
@@ -1,4 +1,5 @@
 import click
+from sqlalchemy.sql import text
 
 from store_inventory.models import Base, engine
 from store_inventory.csv import add_csv_to_database
@@ -9,7 +10,7 @@ def database():
     pass
 
 
-@click.command(help="Create a databae and tables")
+@click.command(help="Create a database and tables")
 def create():
     click.echo("Creating the database and the tables")
     Base.metadata.create_all(engine)
@@ -19,6 +20,26 @@ def create():
 def populate():
     click.echo("Get data from the CSV and populate the Database")
     add_csv_to_database()
+
+
+@click.command(help="Clean duplicate rows within a table")
+@click.option("--name",
+        prompt="Name of the table", 
+        help="The name of the table you want to clean",
+        required=True)
+def clean_table(name):
+    click.echo(f"Cleaning the {name} table")
+
+    try:
+        with engine.connect() as con:
+            if not engine.dialect.has_table(con, name):
+                raise ValueError(f"The table {name} was not found")
+
+            # Add comment
+            statement = text("DELETE t1 FROM product t1 INNER JOIN product t2 WHERE t1.id < t2.id AND t1.name = t2.name;")
+            click.echo(f"{name} table cleaned")
+    except Exception as err:
+        click.echo("Something went wrong when cleaning the table")
 
 
 @click.command(help="Drop the database including all the database inside")
@@ -31,5 +52,6 @@ def drop(force):
 
 database.add_command(create)
 database.add_command(drop)
+database.add_command(clean_table)
 database.add_command(populate)
 
