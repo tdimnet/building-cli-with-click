a138e095f47fa48bc76eccc4bd0a1c65cb827fe0
tdimnet <thomas.dimnet@datadoghq.com>
2022-01-27 14:03:53 +0100

    Add a rule to update the product

--- api.py
+++ api.py
@@ -2,14 +2,17 @@ from flask import Flask, jsonify
 
 from store_inventory.models import session, Product
 
+PRODUCT_READY_TO_SHIP_STATE = 2
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World</p>"
 
 
-@app.route("/product/<id>")
+@app.route("/product/<id>", methods=["GET"])
 def get_product(id):
     product = session.query(Product).filter(Product.id == id).one_or_none()
 
@@ -23,4 +26,17 @@ def get_product(id):
     }
 
     return jsonify(data)
+
+
+@app.route("/product/<id>", methods=["PATCH"])
+def update_product_state(id):
+    product = session.query(Product).filter(Product.id == id).one_or_none()
+
+    if product == None:
+        return "<p>Not found</p>"
+
+    product.product_state_id = PRODUCT_READY_TO_SHIP_STATE
+    session.commit()
+
+    return "<p>Product update</p>"
 
